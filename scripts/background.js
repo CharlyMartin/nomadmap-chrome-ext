89470c0413ff54d484eab14af9ea3cc9a828b0bf
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-15 15:45:34 +1000

    basic flow working

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,43 @@
-// console.log("Start");
+// 1. Variables
+const alarm = 'Daily Location Update';
+let username = localStorage.getItem("username");
+
+// 2. Functions
+function createAlarm() {
+console.log(arguments.callee.name);
+
+ chrome.alarms.create(alarm, {
+   periodInMinutes: (0.1)
+ });
+};
+
+function getBrowserLocation() {
+  console.log(arguments.callee.name);
+
+  if (!navigator.geolocation){
+    alert("Geolocation is not supported by your browser");
+    return;
+  }
+  navigator.geolocation.getCurrentPosition(success, error)
+};
+
+function success(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  console.log(latitude, longitude);
+};
+
+function error() {
+  alert("You did not give access to your position")
+};
+
+function fetch(lat, lgn) {
+
+};
+
+function empty(stuff) {
+  if (stuff === null) {return true};
+};
 
 // chrome.alarms.create("daily-location-update", {
 //     "periodInMinutes": 60 * 12
@@ -42,11 +81,12 @@
 //     });
 // }
 
+// 3. Events
+chrome.alarms.onAlarm.addListener(getBrowserLocation);
 
-// chrome.runtime.onInstalled.addListener(function(details){
-//     if (details.reason == "install"){
-//         chrome.tabs.create({
-//             url: "options.html"
-// 	});
-//     }
-// });
+chrome.runtime.onInstalled.addListener(function(details){
+    if (empty(username)) {
+      chrome.tabs.create({url: "pages/options.html"});
+    };
+    createAlarm();
+});
