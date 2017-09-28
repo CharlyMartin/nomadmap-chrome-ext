0048ffa658562550ebbc2d85a3754894246bf63f
Charly MARTIN <charly.martin.1207@gmail.com>
2017-09-28 10:50:16 +1000

    new alarm.js file

---
+++ scripts/alarm.js
@@ -0,0 +1,73 @@
+// 1. Variables
+const alarm = 'Daily Location Update',
+      optionPage = 'pages/options.html';
+
+let username = localStorage.getItem('username'),
+    email = localStorage.getItem('email'),
+    token = localStorage.getItem('username');
+
+
+// 2. Functions
+function createAlarm() {
+  console.log(arguments.callee.name);
+
+ chrome.alarms.create(alarm, {
+   periodInMinutes: (1)
+   // periodInMinutes: (60 * 12)
+ });
+};
+
+function getBrowserLocation() {
+  console.log(arguments.callee.name);
+
+  if (!navigator.geolocation){
+    alert("Geolocation is not supported by your browser");
+    return;
+  }
+  navigator.geolocation.getCurrentPosition(success, error);
+};
+
+function success(response) {
+  let lat = response.coords.latitude;
+  let lgn = response.coords.longitude;
+  updateAPI(lat, lgn);
+};
+
+function error() {
+  alert("You did not give us access to your position.")
+};
+
+function updateAPI(lat, lgn) {
+  payload = JSON.stringify({
+    nomad: {
+      latitude: lat,
+      longitude: lgn
+    }
+  });
+
+  window.fetch(`https://www.nomadmap.co/api/v1/nomads/${username}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-User-Token': `${token}`,
+      'X-User-Email': `${email}`
+    },
+    body: payload
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  });
+};
+
+function empty(stuff) {
+  if (stuff === null) {return true};
+};
+
+function open(tab) {
+  chrome.tabs.create({url: tab});
+}
+
+
+// 3. Events
+chrome.alarms.onAlarm.addListener(getBrowserLocation);
